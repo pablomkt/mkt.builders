23c584e95057336158899f11d5450a148233a99d
Pablo Nicolas Petran <pablo.petran@globallogic.com>
2016-02-12 23:39:28 -0300

    Initial commit of the main files

---
+++ dev/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dev', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
