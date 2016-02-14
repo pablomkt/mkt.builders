921e7950d93485507daa3b42777d0433c86ef567
Pablo Nicolas Petran <pablo.petran@globallogic.com>
2016-02-14 04:35:44 -0300

    First version that went LIVE

--- dev/src/app/components/navbar/navbar.directive.js
+++ dev/src/app/components/navbar/navbar.directive.js
@@ -1,9 +1,25 @@
-(function() {
+(function () {
   'use strict';
-
   angular
     .module('dev')
-    .directive('acmeNavbar', acmeNavbar);
+    .directive('scrollOnClick', function () {
+      return {
+        restrict: 'A',
+        link: function (scope, $elm, attrs) {
+          var idToScroll = attrs.href;
+          $elm.on('click', function () {
+            var $target;
+            if (idToScroll) {
+              $target = $(idToScroll);
+            } else {
+              $target = $elm;
+            }
+            $("body").animate({scrollTop: $target.offset().top}, "slow");
+          });
+        }
+      }
+    })
+    .directive('mktNavbar', acmeNavbar);
 
   /** @ngInject */
   function acmeNavbar() {
@@ -11,7 +27,7 @@
       restrict: 'E',
       templateUrl: 'app/components/navbar/navbar.html',
       scope: {
-          creationDate: '='
+        creationDate: '='
       },
       controller: NavbarController,
       controllerAs: 'vm',
